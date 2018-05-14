6079a71b6f0e05bc0185764677e9cf19d9a88103
Lingling Kong <wemmaling@163.com>
2018-05-14 23:02:44 +0800

    review代码。删除额外的Actions

--- app/action.js
+++ app/action.js
@@ -1,15 +1,8 @@
 // 静态背景和动态物块的合并更新
-export const MERGE_MAP = 'MERGE_MAP'
 export const UPDATE_MAP = 'UPDATA_MAP'
 // 正在下落过程中的Tetromino的移动
-export const MOVE_TETROMINO = 'MOVE_TETROMINO'
-export const CHANGE_TETROMINO = 'CHANGE_TETROMINO'
 export const UPDATE_TETROMINO = 'UPDATE_TETROMINO'
-// 合并背景后重新掉落新的Tetromino
-export const DROP_NEW_TETROMINO = 'DROP_NEW_TETROMINO'
 export const UPDATE_NEXT_TETROMINO = 'UPDATE_NEXT_TETROMINO'
-// 消除
-export const CLEAR_LINES = 'CLEAR_LINES'
 // 更新分数
 export const UPDATE_SCORE = 'UPDATE_SCORE'
 // 游戏结束
@@ -17,7 +10,6 @@ export const GAME_OVER = 'GAME_OVER'
 // 旋转物块
 export const RORATE = 'RORATE'
 // 改变下落速度
-export const CHANGE_SPEED = 'CHANGE_SPEED'
 export const UPDATE_SPEED = 'UPDATE_SPEED'
 // 长按按键
 export const LR_KEY_UP = 'LR_KEY_UP'
--- app/saga.js
+++ app/saga.js
@@ -2,6 +2,7 @@ import {
   put,
   fork,
   takeEvery,
+  takeLatest,
   select,
   take,
   race,
@@ -16,24 +17,18 @@ import { COL } from './constants'
 
 export default function* rootSaga() {
   yield fork(watchGameStatus)
-  yield takeEvery(A.MOVE_TETROMINO, move)
-  yield takeEvery(A.MERGE_MAP, mergeMap)
-  yield takeEvery(A.DROP_NEW_TETROMINO, dropNewOne)
-  yield takeEvery(A.CLEAR_LINES, clearLines)
-  yield takeEvery(A.RORATE, rorate)
-  yield takeEvery(A.DROP_DIRECTLY, dropDirectly)
   yield takeEvery(A.GAME_OVER, gameOver)
-  yield takeEvery(A.CHANGE_TETROMINO, changeTetromino)
-  yield takeEvery(A.CHANGE_SPEED, changeSpeed)
-  yield takeEvery(A.HOLD_TETROMINO, hold)
 }
 
 function* watchGameStatus() {
+  yield takeLatest(A.UPDATE_SPEED, dropLoop)
+  yield put({ type: A.UPDATE_SPEED, speed: 1 })
+
   while (true) {
     yield take([A.CONTINUE, A.RESTART])
     yield race([
+      gameActions(),
       // 物块自动下落
-      call(dropLoop),
       // 监听下键的按下与释放
       call(dropKeyUpAndDown),
       // 监听左右按键的按下与释放
@@ -41,6 +36,12 @@ function* watchGameStatus() {
       take([A.PAUSE, A.GAME_OVER]),
     ])
   }
+}
+
+function* gameActions() {
+  yield takeEvery(A.RORATE, rorate)
+  yield takeEvery(A.DROP_DIRECTLY, dropDirectly)
+  yield takeEvery(A.HOLD_TETROMINO, handleHold)
 }
 
 // 监听下键的按下与释放
@@ -53,10 +54,10 @@ function* dropKeyUpAndDown() {
       type: A.UPDATE_SPEED,
       speed: 20 * speed,
     })
-    yield race([
-      call(dropLoop),
-      take([A.DROP_KEY_UP, A.GAME_OVER]),
-    ])
+    // yield race([
+    //   call(dropLoop, 'dropKeyUpAndDown'),
+    yield take([A.DROP_KEY_UP, A.GAME_OVER])
+    // ])
     yield put({
       type: A.UPDATE_SPEED,
       speed: speed,
@@ -67,10 +68,6 @@ function* dropKeyUpAndDown() {
 // 监听左右按键的按下与释放
 function* lrKeyUpAndDown() {
   while (true) {
-    const state = yield select()
-    if (state.get('isGameOver')) {
-      break
-    }
     const action = yield take(A.LR_KEY_DOWN)
     const { dRow, dCol } = action
     yield race([
@@ -83,11 +80,7 @@ function* lrKeyUpAndDown() {
 // 加速左右移动
 function* moveLeftOrRight(dRow, dCol) {
   while (true) {
-    yield put({
-      type: A.MOVE_TETROMINO,
-      dRow,
-      dCol,
-    })
+    yield move({ dRow, dCol })
     yield delay(130)
   }
 }
@@ -97,20 +90,14 @@ function* dropLoop() {
   while (true) {
     const state = yield select()
     const { speed, level } = state.toObject()
-    if (!state.get('isGameOver')) {
+    yield move({ dRow: 1, dCol: 0, })
+    if (speed > (level + 1) * 0.5) {
       yield put({
-        type: A.MOVE_TETROMINO,
-        dRow: 1,
-        dCol: 0,
+        type: A.UPDATE_SCORE,
+        getScore: 1,
       })
-      if (speed > (level + 1) * 0.5) {
-        yield put({
-          type: A.UPDATE_SCORE,
-          getScore: 1,
-        })
-      }
     }
-    yield delay(1000 / state.get('speed'))
+    yield delay(1000 / speed)
   }
 }
 
@@ -118,25 +105,20 @@ function* dropLoop() {
 function* move({ dRow, dCol }) {
   // console.log('move-tetromino')
   const state = yield select()
-  const { tetrisMap, curTetromino, isGameOver } = state.toObject()
+  const { tetrisMap, curTetromino } = state.toObject()
   const { row, col } = curTetromino.toObject()
   const nextPosition = curTetromino.update('row', v => v + dRow).update('col', v => v + dCol)
   const canMove = canTetrominoMove(tetrisMap, nextPosition)
   // 如果物块刚出来就发现不能继续移动，Game Over!
   // todo 还存在一些问题
   if (!canMove && row < 0 && dRow === 1) {
-    yield put({ type: A.GAME_OVER })
+    yield gameOver()
   } else {
     // 如果物块不能继续移动且游戏并没有结束，并且动作为向下移动，就合并背景
-    if (!canMove && !isGameOver && dRow === 1) {
-      yield put({
-        type: A.MERGE_MAP
-      })
+    if (!canMove && dRow === 1) {
+      yield mergeMap()
     } else if (canMove) { // 保证物体在下个位置不会碰到墙壁或与其他物块碰撞
-      yield put({
-        type: A.CHANGE_TETROMINO,
-        next: nextPosition,
-      })
+      yield changeTetromino({ next: nextPosition })
     }
   }
 }
@@ -160,25 +142,20 @@ function* changeTetromino({ next }) {
 function* dropDirectly() {
   // console.log('drop-directly')
   const state = yield select()
-  const { tetrisMap, curTetromino, isGameOver } = state.toObject()
-  if (!isGameOver) {
-    yield put({
-      type: A.CHANGE_TETROMINO,
-      next: forecastPosition(tetrisMap, curTetromino)
-    })
-    yield put({
-      type: A.UPDATE_SCORE,
-      getScore: 2 * (19 - curTetromino.get('row')),
-    })
-    yield put({ type: A.MERGE_MAP })
-  }
+  const { tetrisMap, curTetromino } = state.toObject()
+  yield changeTetromino({ next: forecastPosition(tetrisMap, curTetromino) })
+  yield put({
+    type: A.UPDATE_SCORE,
+    getScore: 2 * (19 - curTetromino.get('row')),
+  })
+  yield mergeMap()
 }
 
 // 当物块落下到背景边缘或者碰到其他物体时，动态物块融入背景板
 function* mergeMap() {
   // console.log('merge-map')
   const state = yield select()
-  const { tetrisMap, curTetromino, isGameOver } = state.toObject()
+  const { tetrisMap, curTetromino } = state.toObject()
   const { type, row, col, direction } = curTetromino.toObject()
   const delta = directionMapDelta.get(type).get(direction)
   let newMap = tetrisMap
@@ -189,20 +166,12 @@ function* mergeMap() {
       newMap = newMap.update(row + dRow, value => value.update(col + dCol, () => type))
     }
   }
-  // 如果游戏结束，则不继续掉落新的物块
-  if (!isGameOver) {
-    yield put({
-      type: A.UPDATE_MAP,
-      newMap,
-    })
-
-    yield put({
-      type: A.DROP_NEW_TETROMINO,
-    })
-    yield put({
-      type: A.CLEAR_LINES,
-    })
-  }
+  yield put({
+    type: A.UPDATE_MAP,
+    newMap,
+  })
+  yield dropNewOne()
+  yield clearLines()
 }
 
 // 与背景板融合后判断是否有行满足消除的要求并更新Map
@@ -226,9 +195,7 @@ function* clearLines() {
       type: A.UPDATE_SCORE,
       getScore: scoreRule.get(result - 1),
     })
-    yield put({
-      type: A.CHANGE_SPEED,
-    })
+    yield changeSpeed()
   }
 }
 
@@ -238,7 +205,7 @@ function* changeSpeed() {
   if (score >= 1000 + level * (level - 1) * 500) {
     yield put({
       type: A.UPDATE_SPEED,
-      speed: (level + 1) * 0.5,
+      speed: (level + 2) * 0.5,
     })
     yield put({
       type: A.UPDATE_LEVEL,
@@ -250,17 +217,12 @@ function* changeSpeed() {
 function* dropNewOne() {
   // console.log('drop-new-tetromino')
   const state = yield select()
-  const { isGameOver, nextTetromino } = state.toObject()
-  if (!isGameOver) {
-    yield put({
-      type: A.CHANGE_TETROMINO,
-      next: nextTetromino,
-    })
-    yield put({
-      type: A.UPDATE_NEXT_TETROMINO,
-      next: dropRandom(),
-    })
-  }
+  const { nextTetromino } = state.toObject()
+  yield changeTetromino({ next: nextTetromino })
+  yield put({
+    type: A.UPDATE_NEXT_TETROMINO,
+    next: dropRandom(),
+  })
 }
 
 // 旋转
@@ -271,10 +233,7 @@ function* rorate() {
   const nextPosition = curTetromino.update('direction', v => (v + rorateDir) % 4)
   const canMove = canTetrominoMove(tetrisMap, nextPosition)
   if (canMove && curTetromino.get('type') !== 'O') {
-    yield put({
-      type: A.CHANGE_TETROMINO,
-      next: nextPosition,
-    })
+    yield changeTetromino({ next: nextPosition })
   }
 }
 
@@ -289,7 +248,7 @@ function* gameOver() {
   yield put({ type: A.UPDATE_GAME_STATUS })
 }
 
-function* hold() {
+function* handleHold() {
   const state = yield select()
   const { curTetromino, hold } = state.toObject()
   if (curTetromino.get('canBeHold')) {
@@ -298,15 +257,10 @@ function* hold() {
       hold: curTetromino.get('type'),
     })
     if (hold == null) {
-      yield put({
-        type: A.DROP_NEW_TETROMINO,
-      })
+      yield dropNewOne()
     } else {
       const { row, col } = convert(hold)
-      yield put({
-        type: A.CHANGE_TETROMINO,
-        next: Map({ type: hold, row, col, direction: 0, canBeHold: false }),
-      })
+      yield changeTetromino({ next: Map({ type: hold, row, col, direction: 0, canBeHold: false }) })
     }
   }
 }
